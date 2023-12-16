1480d2c32ddc0fdf779fc1e5e29c071dd422a97d
quantiota <bouarfa.mahi@gmail.com>
2023-12-16 23:48:11 +0100

    update

--- app/config.py
+++ app/config.py
@@ -32,7 +32,7 @@ class Config(object):
     # Mail Authentication
     MAIL_USERNAME = os.getenv('MAIL_USERNAME')
     MAIL_PASSWORD = os.getenv('MAIL_PASSWORD')
-    MAIL_USE_TLS = os.getenv('MAIL_USE_TLS', 'False') == 'True'
+    MAIL_USE_TLS = os.getenv('MAIL_USE_TLS', 'False') == 'False'
     MAIL_USE_SSL = os.getenv('MAIL_USE_SSL', 'False') == 'True'
 
     # Mail Accounts
