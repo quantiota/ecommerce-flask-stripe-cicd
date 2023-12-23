8a4b07873e455c008e8de89e729ed9717fe6e40c
quantiota <bouarfa.mahi@gmail.com>
2023-12-23 19:43:39 +0100

    update

--- app/views.py
+++ app/views.py
@@ -80,6 +80,15 @@ def thank_you():
 ###############################################
 # AUTH 
 
+@app.route('/register', methods=['GET', 'POST'])
+def sign_up():
+    if request.method == 'POST':
+        # Process the form data for registration
+    return render_template('pages/signup_template.html')
+
+
+
+
 @app.route('/login/', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
