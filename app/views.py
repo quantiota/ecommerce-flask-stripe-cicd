05e6e7d9ad8dcbcc7707be3134a62cf9700b2038
quantiota <bouarfa.mahi@gmail.com>
2024-01-05 20:37:40 +0100

    update

--- app/views.py
+++ app/views.py
@@ -145,7 +145,8 @@ def create_checkout_session(path):
                     "amount": product.price * 100,
                 }
             ],  
-            billing_address_collection="required"
+            billing_address_collection="required",         
+            tax_id_collection="enabled": True}
         )
         return jsonify({"sessionId": checkout_session["id"]})
     except Exception as e:
