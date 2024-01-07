beade642c34f11444e57807e8d716e625657156b
quantiota <bouarfa.mahi@gmail.com>
2024-01-07 19:57:14 +0100

    update

--- app/display_server_address.py
+++
@@ -1,8 +0,0 @@
-from config import Config  # Import your Config class from config.py
-
-def main():
-    server_address = Config.SERVER_ADDRESS
-    print(f'Server Address: {server_address}')
-
-if __name__ == '__main__':
-    main()
--- app/views.py
+++ app/views.py
@@ -143,8 +143,6 @@ def create_checkout_session(path):
                     "name": product.name,
                     "quantity": 1,
                     "currency": product.currency,
-                   
-
                     "amount": product.price * 100,
                 }
             ],  
